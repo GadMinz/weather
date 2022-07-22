1ae0f3c18afc9b2970d2396762d4291cc67e244f
GadMinz <baykov97@mail.ru>
2022-07-22 15:40:39 +0300

    Add ThisDayInfo component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import s from "./Home.module.scss";
 import ThisDay from "./components/ThisDay/ThisDay";
+import ThisDayInfo from "./components/ThisDayInfo/ThisDayInfo";
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
     <div className={s.home}>
-      <ThisDay />
+      <div className={s.wrapper}>
+        <ThisDay />
+        <ThisDayInfo />
+      </div>
     </div>
   );
 };
